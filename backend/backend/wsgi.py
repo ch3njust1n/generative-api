997c6a7df97c0943fcc257728b00e1817c234a3a
Justin Chen <ch3njus@gmail.com>
2023-03-19 12:23:04 -0400

    feat: delete media file which contains temporary files before server shutsdown

--- backend/backend/wsgi.py
+++ backend/backend/wsgi.py
@@ -8,9 +8,17 @@ https://docs.djangoproject.com/en/4.1/howto/deployment/wsgi/
 """
 
 import os
-
+import shutil
+import atexit
 from django.core.wsgi import get_wsgi_application
 
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "backend.settings")
 
 application = get_wsgi_application()
+
+def shutdown_handler():
+    print("Executing tasks before shutting down the server...")
+    print(os.getcwd())
+    shutil.rmtree("media/")
+
+atexit.register(shutdown_handler)
