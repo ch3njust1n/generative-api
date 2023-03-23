29c97906005f38701b9551e85b075a3066df558f
Justin Chen <ch3njus@gmail.com>
2023-03-23 17:21:30 -0400

    style: saving progress

--- backend/ingestion/server/server/api/filesystem.py
+++ backend/ingestion/server/server/api/filesystem.py
@@ -1,3 +1,10 @@
+import os
+import time
+from collections import deque
+from typing import Dict, List, Union
+from dotenv import load_dotenv
+import psutil
+import platform
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
@@ -7,12 +14,9 @@ POST configurations to:
 1. Update connected APIs
 2. Modify refresh interval
 """
-
-
 def configure(req: HttpRequest) -> HttpResponse:
     if req.method == "POST":
         configuration = req.POST.get("config")
-
 
 def get_urls():
     return [
