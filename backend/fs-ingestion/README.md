1cce777fa0293e1e55f4c6797058b257c585289b
Justin Chen <ch3njus@gmail.com>
2023-03-23 15:16:21 -0400

    chore: added docker and docker-compose, added env vars and neo4j

--- backend/fs-ingestion/README.md
+++ backend/fs-ingestion/README.md
@@ -1,1 +1,10 @@
-### File System Ingestion Service
+## File System Ingestion Service
+
+
+### Development
+
+Install Neo4J on OSX:
+
+```bash
+brew install neo4j
+```
---
+++ backend/fs-ingestion/server/server/api/filesystem.py
@@ -0,0 +1,23 @@
+from django.contrib import admin
+from django.urls import path
+from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
+
+
+def 
+
+
+'''
+POST configurations to:
+1. Update connected APIs
+2. Modify refresh interval
+'''
+def configure(req: HttpRequest) -> HttpResponse:
+    if req.method == "POST":
+        configuration = req.POST.get("config")
+
+
+def get_urls():
+    return [
+        path('admin/', admin.site.urls),
+        path('api/v1/configure', configure, name='configure')
+    ]
