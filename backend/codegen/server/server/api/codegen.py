079f5d51fc051825f0ce8e4bf1a764c3e37ba8b5
Justin Chen <ch3njus@gmail.com>
2023-04-04 09:05:24 -0400

    chore: saving progress on codegen api

--- backend/codegen/server/server/api/codegen.py
+++ backend/codegen/server/server/api/codegen.py
@@ -1,3 +1,5 @@
+import multiprocessing as mp
+
 from django.contrib import admin
 from django.urls import path
 from django.http import HttpRequest, HttpResponse, HttpResponseBadRequest, JsonResponse
@@ -11,22 +13,27 @@ def generate_tests():
     pass
 
 
-def analyze():
+def format(data: dict) -> dict:
     pass
 
 
-def load():
+def load(id: str) -> dict:
     pass
 
 
-def run():
-    pass
+def run(id: str) -> None:
+    data = load(id)
+    formatted = format(data)
+    pipe = []
 
 
+'''
+Calls run with the given id
+'''
 def start_job(req: HttpRequest) -> HttpResponse:
     if req.method == "POST":
-        id = req.POST.get("id")
-    pass
+        run(req.POST.get("id"))
+        
 
 
 urlpatterns = [
