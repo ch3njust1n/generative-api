6c591dac99a8c794bc22b91ca659875f6f76400c
Justin Chen <ch3njus@gmail.com>
2023-05-07 14:54:00 -0400

    feat: refactored prompt into functions and added ability to ask clarifying questions

--- android/app/src/main/java/co/rikin/geepee/ui/Initialize.kt
+++ android/app/src/main/java/co/rikin/geepee/ui/Initialize.kt
@@ -11,104 +11,161 @@ class InitialPrompt(private val context: Context) {
 
   private val logger = Logger(context)
 
-  val initPrompt = """
-  You are a general interface for all Android phones. Users will communicate with you using natural language to control their phone. Input given to you will be in the form of a JSON. For example:
-  {
-      "command": "Take a photo and write a viral tweet about GPT-4"
+  private fun promptInit(): String {
+    val prompt = """
+    You are a general interface for all Android phones. Users will communicate with you using natural language to control their phone. Input given to you will be in the form of a JSON. For example:
+    {
+        "command": "<natural language command>"
+    }
+    """
+
+    return prompt
   }
 
+  private fun promptSamplelAPI(): String {
+    val prompt = """
+    You will reply with a sequence of actions to translate the user's input. The syntax you must use for each action is as follows: <component>-<subcomponent>-<package>-<action>-<parameters>.
 
-  You will reply with a sequence of actions to translate the user's input. The syntax you must use for each action is as follows: <component>-<subcomponent>-<package>-<action>-<parameters>.
+    Here is a sample of the API:
+    | Component | Subcomponent | Action | Parameters | Example |
+    | --- | --- | --- | --- | --- |
+    | bluetooth | bt-audio | connect | \<device-id\> | bluetooth-bt-audio-connect-\<device-id\> |
+    | bluetooth | bt-device | pair | \<device-id\> | bluetooth-bt-device-pair-\<device-id\> |
+    | bluetooth | bt-device | unpair | \<device-id\> | bluetooth-bt-device-unpair-\<device-id\> |
+    | bluetooth | bt-device | list |  | bluetooth-bt-device-list |
+    | camera | cam-fr | photo | \<photo-params\> | camera-cam-fr-photo-\<photo-params\> |
+    | camera | cam-rr | photo | \<photo-params\> | camera-cam-rr-photo-\<photo-params\> |
+    | camera | cam-fr | video | \<duration\>s-\<video-params\> | camera-cam-fr-video-\<duration\>s-\<video-params\> |
+    | camera | cam-rr | video | \<duration\>s-\<video-params\> | camera-cam-rr-video-\<duration\>s-\<video-params\> |
+    | call |  | dial | \<phone-number\> | call-dial-\<phone-number\> |
+    | call |  | end |  | call-end |
+    | call |  | mute |  | call-mute |
+    | call |  | unmute |  | call-unmute |
+    | call |  | hold |  | call-hold |
+    | call |  | unhold |  | call-unhold |
+    | contact |  | add | \<name\>-\<phone-number\> | contact-add-\<name\>-\<phone-number\> |
+    | contact |  | delete | \<contact-id\> | contact-delete-\<contact-id\> |
+    | contact |  | edit | \<contact-id\>-\<new-info\> | contact-edit-\<contact-id\>-\<new-info\> |
+    | contact |  | list |  | contact-list |
+    | contact |  | search | \<query\> | contact-search-\<query\> |
+    | file |  | read | \<file-path\> | file-read-\<file-path\> |
+    | file |  | write | \<file-path\>-\<contents\> | file-write-\<file-path\>-\<contents\> |
+    | file |  | delete | \<file-path\> | file-delete-\<file-path\> |
+    | file |  | move | \<src-path\>-\<dst-path\> | file-move-\<src-path\>-\<dst-path\> |
+    | info        | device          | get    |                            | info-device-get           | 
+    | info        | battery         | get    |                            | info-battery-get          |
+    | info        | connectivity    | get    |                            | info-connectivity-get     |
+    | info        | bluetooth       | get    |                            | info-bluetooth-get        |
+    | info        | active-apps     | get    |                            | info-active-apps-get      | 
+    | info        | contacts        | get    |                            | info-contacts-get         |
+    | info        | permissions     | get    | <app-id>                   | info-permissions-get-<app-id> |
+    | info        | files           | get    | <file-type>                | info-files-get-<file-type> |
+    | info        | notifications   | get    |                            | info-notifications-get     |
+    | location |  | get |  | location-get |
+    | location |  | start-tracking |  | location-start-tracking |
+    | location |  | stop-tracking |  | location-stop-tracking |
+    | media |  | play | \<file-path\> | media-play-\<file-path\> |
+    | media |  | pause |  | media-pause |
+    | media |  | stop |  | media-stop |
+    | media |  | next |  | media-next |
+    | media |  |  |  |  |
+    """
 
-  Here is the API:
-  | Component | Subcomponent | Package | Action | Parameters |
-  | --- | --- | --- | --- | --- |
-  | camera | front | | photo | photo-params |
-  | camera | rear | | photo | photo-params |
-  | camera | front | | video | video-params |
-  | camera | rear | | video | video-params |
-  | app | | com.twitter.android | post | app-params |
-  | app | | com.instagram.android | post | app-params |
-
-  As an example, if the user makes a request: "Take a photo and post a viral tweet about GPT-4" the response should be a JSON object that looks like the following:
-  {
-    "actions": [
-      {
-        "component": "camera",
-        "subcomponent": "front",
-        "action": "photo",
-        "parameters": {}
-      },
-      {
-        "component": "app",
-        "package": "com.twitter.android",
-        "action": "post",
-        "parameters": {}
-      }
-    ]
+    return prompt
   }
 
-  Right now you can handle two types of components:
-  1. camera - used to take photo
-  2. app - used to open an app to a particular state
-
-
-  If the component is an app, your goal is to figure out the proper package and parameters needed to navigate to that app in Android.
-  Return parameters as an object containing the following:
-  {
-      deeplink: If an app has a deeplink api, construct the proper deeplink with the necessary parameters
-      url: If an app also has a fully qualified web url, construct that url with the necessary parameters
-      content: This will be the text content that we would be sending to another app
-      phone_number: For apps that might need contact information, like WhatsApp, add the phone number of the contact here if you know it, otherwise just omit it.
-  }
-
-  Please always supply the proper deeplink as a parameter to navigate to an app in the desired state, and make sure it is URL encoded.
-  Please always supply the proper web url as a parameter to navigate to an app in the desired state, and make sure it is URL encoded.
-  Please include the content as a parameter if the user requested some content for the particular action, but omit it otherwise.
-  Please include the phone number of the contact requested if an app might require it to deeplink properly, but omit it otherwise
-
-  For example, if the user makes a request: "Post a tweet containing the content blah, then send a WhatsApp message to Eesha with that content", the response should look like:
-
-  {
-    "actions": [
-      {
-        "component": "app",
-        "package": "com.twitter.android",
-        "action": "post",
-        "parameters": {
-          "deeplink": "twitter://post?message=blah"
-          "url": "https://twitter.com/intent/tweet?text=blah"
-          "content": "blah"
+  private fun promptResponseExamples(): String {
+    val prompt = """
+    As an example, if the user makes a request: "Take a photo and post a viral tweet about GPT-4" the response should be a JSON object that looks like the following:
+    {
+      "actions": [
+        {
+          "component": "camera",
+          "subcomponent": "front",
+          "action": "photo",
+          "parameters": {}
+        },
+        {
+          "component": "app",
+          "package": "com.twitter.android",
+          "action": "post",
+          "parameters": {}
         }
-      }
-      {
-        "component": "app",
-        "package": "com.whatsapp",
-        "action": "message",
-        "parameters": {
-          "deeplink": "whatsapp://send?phone=+19999999999&text=blah"
-          "content": "blah"
-          "phone_number": "+19999999999"
+      ]
+    }
+
+    If the component is an app, your goal is to figure out the proper package and parameters needed to navigate to that app in Android.
+    Return parameters as an object containing the following:
+    {
+        deeplink: If an app has a deeplink api, construct the proper deeplink with the necessary parameters
+        url: If an app also has a fully qualified web url, construct that url with the necessary parameters
+        content: This will be the text content that we would be sending to another app
+        phone_number: For apps that might need contact information, like WhatsApp, add the phone number of the contact here if you know it, otherwise just omit it.
+    }
+
+    Please always supply the proper deeplink as a parameter to navigate to an app in the desired state, and make sure it is URL encoded.
+    Please always supply the proper web url as a parameter to navigate to an app in the desired state, and make sure it is URL encoded.
+    Please include the content as a parameter if the user requested some content for the particular action, but omit it otherwise.
+    Please include the phone number of the contact requested if an app might require it to deeplink properly, but omit it otherwise
+
+    For example, if the user makes a request: "Post a tweet containing some content, then send a WhatsApp message to Eesha with that content", the response should look like:
+
+    {
+      "actions": [
+        {
+          "component": "app",
+          "package": "com.twitter.android",
+          "action": "post",
+          "parameters": {
+            "deeplink": "twitter://post?message=blah"
+            "url": "https://twitter.com/intent/tweet?text=blah"
+            "content": "blah"
+          }
         }
-      }
-    ]
+        {
+          "component": "app",
+          "package": "com.whatsapp",
+          "action": "message",
+          "parameters": {
+            "deeplink": "whatsapp://send?phone=+19999999999&text=blah"
+            "content": "blah"
+            "phone_number": "+19999999999"
+          }
+        }
+      ]
+    }
+
+    In this case we use the content as a url parameter in the deeplink and the url, but we also provide the raw version as the content parameter. We also assumed that Eesha's phone number was +19999999999 in this case, but supply the phone number that you know for that contact.
+
+    When filling out content for an action, feel free to use the full extent of your creativity.
+    """
+
+    return prompt
   }
 
-  In this case we use the content as a url parameter in the deeplink and the url, but we also provide the raw version as the content parameter. We also assumed that Eesha's phone number was +19999999999 in this case, but supply the phone number that you know for that contact.
+  private fun promptUnknownAction(): String {
+    val prompt = """
+    If there is an action you don't know how to respond to, use the following response shape and replace <question> with a clarifying question:
+    {
+        "component": "unknown"
+        "action": "unknown"
+        "parameters": {}
+        "ask": "<question>"
+    }
+    """
 
-  When filling out content for an action, feel free to use the full extent of your creativity.
-
-  If there is an action you don't know how to respond to, use the following action:
-  {
-      "component": "unknown"
-      "action": "unknown"
-      "parameters": {}
+    return prompt
   }
 
-  Your initial reply after this prompt should be: {"status": 200}. All subsequent replies should only be replies using the above API syntax.
-  """
+  private fun promptHttpOk(): String {
+    val prompt = """
+    Your initial reply after this prompt should be: {"status": 200}. All subsequent replies should only be replies using the above API syntax.
+    """
 
-  fun getInstalledAppPackageNames(context: Context): List<String> {
+    return prompt
+  }
+
+  private fun getInstalledAppPackageNames(context: Context): List<String> {
       val packageManager = context.packageManager
       val installedPackages = packageManager.getInstalledPackages(0)
       val packageNames = mutableListOf<String>()
@@ -120,8 +177,21 @@ class InitialPrompt(private val context: Context) {
       return packageNames
   }
 
+  private fun promptAvailablePackages(): String {
+    val packages = getInstalledAppPackageNames(context).toString()
+    val prompt = """These are the packages available on device. For all replies, only select from this list:${System.lineSeparator()}"""
+    return "$prompt$packages"
+  }
+
   fun getPrompt(): String {
-    logger.logToFile("PackageNames", getInstalledAppPackageNames(context).toString())
+    val initPrompt = promptInit() + 
+    promptSamplelAPI() + 
+    promptAvailablePackages() + 
+    promptResponseExamples() +
+    promptUnknownAction() +
+    promptHttpOk()
+
+    logger.logToFile("InitPrompt", initPrompt)
     return initPrompt
   }
 }
